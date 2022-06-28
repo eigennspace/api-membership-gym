e0887fcf230d1e966a69c2e23c687da6a7ce5b89
Harist Islami <islamiharist1998@gmail.com>
2022-06-28 23:00:18 +0700

    Add welcome index

---
+++ src/main/java/com/groupone/membershipgym/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.groupone.membershipgym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
