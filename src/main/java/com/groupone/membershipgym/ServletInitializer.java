fbdda5756883a582feb578503c317837957a0119
Harist Islami <islamiharist1998@gmail.com>
2022-06-28 16:52:44 +0700

    refactor package .war into .jar

--- src/main/java/com/groupone/membershipgym/ServletInitializer.java
+++ src/main/java/com/groupone/membershipgym/ServletInitializer.java
@@ -1,13 +1,13 @@
-package com.groupone.membershipgym;
-
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(MembershipGymApplication.class);
-    }
-
-}
+//package com.groupone.membershipgym;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(MembershipGymApplication.class);
+//    }
+//
+//}
