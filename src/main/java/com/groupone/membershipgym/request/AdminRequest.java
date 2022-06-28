73e17e876f08a854b5b58b50228acc0250b849f6
Harist Islami <islamiharist1998@gmail.com>
2022-06-28 16:58:24 +0700

    Add level admin in request

--- src/main/java/com/groupone/membershipgym/request/AdminRequest.java
+++ src/main/java/com/groupone/membershipgym/request/AdminRequest.java
@@ -1,6 +1,7 @@
 package com.groupone.membershipgym.request;
 
 import com.groupone.membershipgym.entity.Admins;
+import com.groupone.membershipgym.entity.LevelAdmin;
 import com.groupone.membershipgym.utils.ValidPassword;
 import lombok.*;
 
@@ -27,11 +28,13 @@ public class AdminRequest {
     @Email(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
     private String email;
 
+    private LevelAdmin level;
+
     @ValidPassword
     private String password;
 
     public Admins convertToEntity(){
         return Admins.builder().firstName(this.firstName).lastName(this.lastName).
-        userName(this.userName).email(this.email).password(this.password).build();
+        userName(this.userName).email(this.email).level(this.level).password(this.password).build();
     }
 }
